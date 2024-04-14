4e6e01e3e21c3da904c7f195d9bd24d6c9d3ce7b
Mashopy <eliasgheeraert@gmail.com>
2024-04-14 11:44:34 +0200

    ASoC: codecs: mt6357-accdet: Fixup a symbol export
    This symbol is already present at mt6359p-accdet

--- sound/soc/codecs/mt6357-accdet.c
+++ sound/soc/codecs/mt6357-accdet.c
@@ -543,7 +543,7 @@ static int accdet_create_attr(struct device_driver *driver)
 }
 
 /* get plug-in Resister for audio call */
-int accdet_read_audio_res(unsigned int res_value)
+int mt6357_accdet_read_audio_res(unsigned int res_value)
 {
 	pr_info("%s() resister value: R=%u(ohm)\n", __func__, res_value);
 
@@ -562,7 +562,7 @@ int accdet_read_audio_res(unsigned int res_value)
 
 	return RET_GT_5K;
 }
-EXPORT_SYMBOL(accdet_read_audio_res);
+EXPORT_SYMBOL(mt6357_accdet_read_audio_res);
 
 static u64 accdet_get_current_time(void)
 {
